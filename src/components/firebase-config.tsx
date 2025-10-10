fab687fb3ef2ed06e56858c921aa426b6e9b7cdb
Jainam Dedhia <jainam.d@somaiya.edu>
2025-10-10 20:32:56 +0530

    changes

--- src/components/firebase-config.tsx
+++ src/components/firebase-config.tsx
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAntJAPzq075eX8f9cce3VIB0Sw92vmmdM",
+  authDomain: "fostride-f56ea.firebaseapp.com",
+  projectId: "fostride-f56ea",
+  storageBucket: "fostride-f56ea.firebasestorage.app",
+  messagingSenderId: "391340825157",
+  appId: "1:391340825157:web:49df7a979e3cb05bce212c",
+  measurementId: "G-5TZSVCB1HB"
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
